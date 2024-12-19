0c8bbc2cd70f1a8b22c2c22f88349bb519618b61
JuergenReiser <juergen.reiser@neo-q.de>
2024-12-19 08:28:33 +0100

    Add file transfer dependency injection extensions; restructure directory build props

--- backend/src/Services/FileTransfer/WebApi/Program.cs
+++ backend/src/Services/FileTransfer/WebApi/Program.cs
@@ -1,6 +1,16 @@
+using Application;
+using Domain;
+using Infrastructure;
+using Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDomainServices();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
+builder.Services.AddPresentationServices();
 
 var app = builder.Build();
 
