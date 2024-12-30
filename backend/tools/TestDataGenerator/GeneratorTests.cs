2c92ad4c17a3801d0407e7b3cf6eec3c0c750e83
lemao81 <10658892+Lemao81@users.noreply.github.com>
2024-12-30 11:13:35 +0100

    Add test data generator tool

---
+++ backend/tools/TestDataGenerator/GeneratorTests.cs
@@ -0,0 +1,70 @@
+﻿using Domain.Media;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Persistence;
+
+// ReSharper disable Xunit.XunitTestWithConsoleOutput
+
+namespace TestDataGenerator;
+
+public class GeneratorTests
+{
+	[Fact]
+	public async Task ClearMediaData()
+	{
+		await GetDbContext().MediaFolders.ExecuteDeleteAsync();
+	}
+
+	[Fact]
+	public async Task AddMediaFolders()
+	{
+		var dbContext = GetDbContext();
+
+		var userId = new UserId(Guid.NewGuid());
+		var mediaFolders = Enumerable.Range(0, 10)
+			.Select(index =>
+			{
+				var       mediaFolder = MediaFolder.Create(userId);
+				MediaItem mediaItem;
+				if (index % 2 == 0)
+				{
+					mediaItem = MediaItem.Create(mediaFolder.Id, userId, MediaKind.Picture, MediaSizeKind.Original);
+				}
+				else
+				{
+					mediaItem = MediaItem.Create(mediaFolder.Id, userId, MediaKind.Video, MediaSizeKind.Original);
+					mediaItem.AddMetaData("DurationSec", Random.Shared.NextInt64(5, 31));
+				}
+				mediaFolder.AddMediaItem(mediaItem);
+
+				return mediaFolder;
+			})
+			.ToList();
+
+		await dbContext.AddRangeAsync(mediaFolders);
+		await dbContext.SaveChangesAsync();
+	}
+
+	private static FileTransferDbContext GetDbContext()
+	{
+		var configBuilder = new ConfigurationBuilder().AddUserSecrets<GeneratorTests>();
+		var dbPassword    = configBuilder.Build().GetSection("DbPassword").Value;
+
+		var dbBuilder = new DbContextOptionsBuilder<FileTransferDbContext>();
+		dbBuilder.UseNpgsql(new NpgsqlDataSourceBuilder
+			{
+				ConnectionStringBuilder =
+				{
+					Host     = "localhost",
+					Database = "vista_file_transfer",
+					Username = "sa",
+					Password = dbPassword
+				}
+			}.Build())
+			.UseSnakeCaseNamingConvention();
+
+		return new FileTransferDbContext(dbBuilder.Options);
+	}
+}
