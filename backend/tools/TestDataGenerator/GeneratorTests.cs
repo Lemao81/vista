3913448f59c18d0a5b8c00ba8a365cefb2e73b80
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-01-28 15:08:06 +0100

    Generator tool random file name creation

--- backend/tools/TestDataGenerator/GeneratorTests.cs
+++ backend/tools/TestDataGenerator/GeneratorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Security.Cryptography;
+using Bogus;
 using Domain.Media;
 using Domain.Users;
 using MediatR;
@@ -16,6 +17,10 @@ namespace TestDataGenerator;
 [SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped")]
 public class GeneratorTests
 {
+	private readonly (string Extension, string MediaType)[] _pictureMetadata = [("png", "image/png"), ("jpg", "image/jpeg"), ("jpeg", "image/jpeg")];
+	private readonly (string Extension, string MediaType)[] _videoMetadata   = [("mp4", "video/mp4"), ("avi", "video/x-msvideo"), ("mkv", "video/x-matroska")];
+	private readonly Faker                                  _faker           = new();
+
 	[Fact(Skip = "TestDataGenerator")]
 	public async Task ClearMediaData()
 	{
@@ -32,15 +37,14 @@ public class GeneratorTests
 		var mediaFolders = Enumerable.Range(0, 10)
 			.Select(index =>
 			{
-				var       mediaFolder = MediaFolder.Create(userId);
-				MediaItem mediaItem;
-				if (index % 2 == 0)
+				var mediaKind     = index % 2 == 0 ? MediaKind.Picture : MediaKind.Video;
+				var metadataArray = index % 2 == 0 ? _pictureMetadata : _videoMetadata;
+				var (extension, mediaType) = metadataArray[Random.Shared.Next(metadataArray.Length)];
+
+				var mediaFolder = MediaFolder.Create(userId, _faker.System.FileName(extension));
+				var mediaItem   = MediaItem.Create(mediaFolder.Id, userId, mediaKind, MediaSizeKind.Original, mediaType);
+				if (mediaKind == MediaKind.Video)
 				{
-					mediaItem = MediaItem.Create(mediaFolder.Id, userId, MediaKind.Picture, MediaSizeKind.Original);
-				}
-				else
-				{
-					mediaItem = MediaItem.Create(mediaFolder.Id, userId, MediaKind.Video, MediaSizeKind.Original);
 					mediaItem.AddMetaData("DurationSec", RandomNumberGenerator.GetInt32(5, 31));
 				}
 
