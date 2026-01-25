af69c0abd3eabd566ee1f24f5068d698119d193b
lemao81 <10658892+Lemao81@users.noreply.github.com>
2026-01-25 19:24:38 +0100

    test(authentication): add SignUpUserRequestValidator tests

---
+++ backend/test/Unit/Service.Tests/Tests/Authentication/SignUpUser/SignUpUserRequestValidatorTests.cs
@@ -0,0 +1,66 @@
+﻿using Authentication.Presentation.SignUpUser;
+using SharedApi.Authentication.SignUpUser;
+
+namespace Service.Tests.Tests.Authentication.SignUpUser;
+
+public class SignUpUserRequestValidatorTests
+{
+	private readonly SignUpUserRequestValidator _classUnderTest;
+
+	public SignUpUserRequestValidatorTests()
+	{
+		_classUnderTest = new SignUpUserRequestValidator();
+	}
+
+	[Fact]
+	public async Task ValidRequest_Should_ReturnValid()
+	{
+		// Arrange
+		var request = new SignUpUserRequest("user", "test@test.com", "any", "any");
+
+		// Act
+		var result = await _classUnderTest.ValidateAsync(request, TestContext.Current.CancellationToken);
+
+		// Assert
+		await Verify(result);
+	}
+
+	[Fact]
+	public async Task EmptyInputs_Should_BeInvalid()
+	{
+		// Arrange
+		var request = new SignUpUserRequest(string.Empty, string.Empty, string.Empty, string.Empty);
+
+		// Act
+		var result = await _classUnderTest.ValidateAsync(request, TestContext.Current.CancellationToken);
+
+		// Assert
+		await Verify(result);
+	}
+
+	[Fact]
+	public async Task InvalidEmail_Should_BeInvalid()
+	{
+		// Arrange
+		var request = new SignUpUserRequest("user", "test.com", "any", "any");
+
+		// Act
+		var result = await _classUnderTest.ValidateAsync(request, TestContext.Current.CancellationToken);
+
+		// Assert
+		await Verify(result);
+	}
+
+	[Fact]
+	public async Task UnequalPasswordRepeat_Should_BeInvalid()
+	{
+		// Arrange
+		var request = new SignUpUserRequest("user", "test@test.com", "any", "other");
+
+		// Act
+		var result = await _classUnderTest.ValidateAsync(request, TestContext.Current.CancellationToken);
+
+		// Assert
+		await Verify(result);
+	}
+}
