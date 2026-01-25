fe6ef6a814999736520dd2c95cff090acf94319a
lemao81 <10658892+Lemao81@users.noreply.github.com>
2026-01-25 19:02:04 +0100

    test(authentication): add authentication db context migration tests

---
+++ backend/test/Unit/Service.Tests/Tests/Authentication/AuthenticationDbContextTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Service.Tests.Utilities;
+
+namespace Service.Tests.Tests.Authentication;
+
+public class AuthenticationDbContextTests
+{
+	[Fact]
+	public async Task GenerateScript_Should_GenerateProperSchema()
+	{
+		// Arrange
+		await using var fixture = new AuthenticationDbContextFixture();
+
+		// Act
+		var script = fixture.DbContext.Database.GenerateCreateScript();
+
+		// Assert
+		await Verify(script);
+	}
+
+	[Fact]
+	public async Task Migrations_Should_BeUpToDate()
+	{
+		// Arrange
+		await using var fixture = new AuthenticationDbContextFixture();
+
+		var migrationModelDiffer    = fixture.DbContext.GetService<IMigrationsModelDiffer>();
+		var migrationsAssembly      = fixture.DbContext.GetService<IMigrationsAssembly>();
+		var modelRuntimeInitializer = fixture.DbContext.GetService<IModelRuntimeInitializer>();
+		var designTimeModel         = fixture.DbContext.GetService<IDesignTimeModel>();
+
+		var currentModel = designTimeModel.Model;
+
+		var snapshotModel = migrationsAssembly.ModelSnapshot?.Model;
+		if (snapshotModel is IMutableModel mutableModel)
+		{
+			snapshotModel = mutableModel.FinalizeModel();
+		}
+
+		if (snapshotModel is not null)
+		{
+			snapshotModel = modelRuntimeInitializer.Initialize(snapshotModel);
+		}
+
+		// Act
+		var modelDifferences = migrationModelDiffer.GetDifferences(source: snapshotModel?.GetRelationalModel(), target: currentModel.GetRelationalModel());
+
+		// Assert
+		Assert.Empty(modelDifferences);
+	}
+}
---
+++ backend/test/Unit/Service.Tests/Utilities/AuthenticationDbContextFixture.cs
@@ -0,0 +1,18 @@
+﻿using Authentication.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service.Tests.Utilities;
+
+public sealed class AuthenticationDbContextFixture : IAsyncDisposable
+{
+	public AuthenticationDbContextFixture()
+	{
+		var dbOptionsBuilder = new DbContextOptionsBuilder<AuthenticationDbContext>();
+		dbOptionsBuilder.UseNpgsql().UseSnakeCaseNamingConvention();
+		DbContext = new AuthenticationDbContext(dbOptionsBuilder.Options);
+	}
+
+	public AuthenticationDbContext DbContext { get; }
+
+	public async ValueTask DisposeAsync() => await DbContext.DisposeAsync();
+}
